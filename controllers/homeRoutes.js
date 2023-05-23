ff515cfe9087c6ed402365c8e1706a442bcc5210
Sean Hayes <hayessea000@outlook.com>
2023-05-22 21:32:19 -0500

    got dashboard to load

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -23,56 +23,56 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/post/:id', async (req, res) => {
-  try {
-    const userPostData = await UserPost.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: { exclude: ['password'] },
-        },
-      ],
-    });
+// router.get('/post/:id', async (req, res) => {
+//   try {
+//     const userPostData = await UserPost.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: { exclude: ['password'] },
+//         },
+//       ],
+//     });
 
-    const userPost = userPostData.get({ plain: true });
+//     const userPost = userPostData.get({ plain: true });
 
-    res.render('post', {
-      ...userPost,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('post', {
+//       ...userPost,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-// Use withAuth middleware to prevent access to route
-router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: UserPost }],
-    });
+// // Use withAuth middleware to prevent access to route
+// router.get('/dashboard', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: UserPost }],
+//     });
 
-    const user = userData.get({ plain: true });
+//     const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('dashboard', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/dashboard');
-    return;
-  }
+// router.get('/login', (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/dashboard');
+//     return;
+//   }
 
-  res.render('login');
-});
+//   res.render('login');
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
+const hbs = exphbs.create({ helpers });
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
